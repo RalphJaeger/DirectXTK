b8632a1d10513f2dade75357c1c42390fc755034
jae <r.jaeger@eks-informatik.com>
2023-01-24 12:37:01 +0100

    DirectXTK

--- Src/WICTextureLoader.cpp
+++ Src/WICTextureLoader.cpp
@@ -186,7 +186,7 @@ namespace DirectX
 {
     inline namespace DX11
     {
-        namespace Internal
+        namespace Internal2
         {
             bool IsWIC2() noexcept;
             IWICImagingFactory* GetWIC() noexcept;
@@ -195,12 +195,12 @@ namespace DirectX
     }
 }
 
-bool DirectX::DX11::Internal::IsWIC2() noexcept
+bool DirectX::DX11::Internal2::IsWIC2() noexcept
 {
     return g_WIC2;
 }
 
-IWICImagingFactory* DirectX::DX11::Internal::GetWIC() noexcept
+IWICImagingFactory* DirectX::DX11::Internal2::GetWIC() noexcept
 {
     static INIT_ONCE s_initOnce = INIT_ONCE_STATIC_INIT;
 
@@ -217,7 +217,7 @@ IWICImagingFactory* DirectX::DX11::Internal::GetWIC() noexcept
     return factory;
 }
 
-using namespace DirectX::DX11::Internal;
+using namespace DirectX::DX11::Internal2;
 
 namespace
 {
